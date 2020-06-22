2b6f93ff632fc49ae5a004c7e2632db2f55a0a1d
victor raji <vheektoure@gmail.com>
2020-06-22 18:19:50 +0100

    fixed untracked files

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const http = require('http');
 const app = require('./app');
 
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(__dirname+'/public/'))
-//     app.get(/.*/, (req,res) => res.sendFile(__dirname+ '/public/index.html'));
-// }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname+'/public/'))
+    app.get(/.*/, (req,res) => res.sendFile(__dirname+ '/public/index.html'));
+}
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
